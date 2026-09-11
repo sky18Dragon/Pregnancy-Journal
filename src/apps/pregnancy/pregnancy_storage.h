d35de4b90cbdcfaaed72625a05e989167db56408
张惠龙 <zhanghuilong@xinpayroll.com>
2026-09-11 22:11:31 +0800

    refactor: remove pregnancy app

--- src/apps/pregnancy/pregnancy_storage.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "esp_err.h"
-#include "pregnancy_state.h"
-
-esp_err_t pregnancy_storage_load(PregnancyDate &due_date, bool &found);
-esp_err_t pregnancy_storage_save(const PregnancyDate &due_date);
